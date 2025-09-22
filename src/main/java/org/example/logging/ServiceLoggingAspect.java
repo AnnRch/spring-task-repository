e245a11e0e34e96dd6f223219b9ea172e1458fae
Anna Rackovych <rackovych.anna@gmail.com>
2025-09-22 06:17:39 +0300

    ServiceLggingAspect and FacadeLoggingAspect created feature

---
+++ src/main/java/org/example/logging/ServiceLoggingAspect.java
@@ -0,0 +1,40 @@
+package org.example.logging;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+@Slf4j
+@Aspect
+@Component
+public class ServiceLoggingAspect {
+
+    @Pointcut("execution(public * org.example.service..*(..))")
+    public void serviceMethods(){}
+
+    @Before("serviceMethods")
+    public void logMethodCall(JoinPoint joinPoint){
+        String methodName = joinPoint.getSignature().toShortString();
+        Object[] args = joinPoint.getArgs();
+        log.info("Calling method: {} with arguments {}", methodName, Arrays.toString(args));
+    }
+
+    @AfterReturning(pointcut = "serviceMethods()", returning = "result")
+    public void logMethodReturn(JoinPoint joinPoint, Object result) {
+        String methodName = joinPoint.getSignature().toShortString();
+        log.info("Method {} returned with value: {}", methodName, result);
+    }
+
+
+    @AfterThrowing(pointcut = "serviceMethods()", throwing = "ex")
+    public void logMethodException(JoinPoint joinPoint, Throwable ex) {
+        String methodName = joinPoint.getSignature().toShortString();
+        log.error("Method {} threw exception: {}", methodName, ex.getMessage(), ex);
+    }
+}
